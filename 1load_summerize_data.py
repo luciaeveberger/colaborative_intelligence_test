0c4bb5e601f167e4a8402ecafd4efda4eb328a05
lberger1993 <berger.luciaeve@gmail.com>
2018-10-14 14:52:13 +0200

    Adds details of the files

--- 1load_summerize_data.py
+++ 1load_summerize_data.py
@@ -4,9 +4,10 @@ from numpy import vstack
 from pandas import read_csv
 from matplotlib import pyplot
 
+
 # return list of traces, and arrays for targets, groups and paths
 def load_dataset(prefix='IndoorMovement/'):
-    grps_dir, data_dir = prefix+'groups/', prefix+'dataset/'
+    grps_dir, data_dir = prefix + 'groups/', prefix + 'dataset/'
     # load mapping files
     targets = read_csv(data_dir + 'MovementAAL_target.csv', header=0)
     groups = read_csv(grps_dir + 'MovementAAL_DatasetGroup.csv', header=0)
@@ -21,21 +22,22 @@ def load_dataset(prefix='IndoorMovement/'):
         df = read_csv(filename, header=0)
         values = df.values
         sequences.append(values)
-    return sequences, targets.values[:,1], groups.values[:,1], paths.values[:,1]
+    return sequences, targets.values[:, 1], groups.values[:, 1], paths.values[:, 1]
+
 
 # load dataset
 sequences, targets, groups, paths = load_dataset()
 # summarize class breakdown
-class1,class2 = len(targets[targets==-1]), len(targets[targets==1])
-print('Class=-1: %d %.3f%%' % (class1, class1/len(targets)*100))
-print('Class=+1: %d %.3f%%' % (class2, class2/len(targets)*100))
+class1, class2 = len(targets[targets == -1]), len(targets[targets == 1])
+print('Class=-1: %d %.3f%%' % (class1, class1 / len(targets) * 100))
+print('Class=+1: %d %.3f%%' % (class2, class2 / len(targets) * 100))
 # histogram for each anchor point
 all_rows = vstack(sequences)
 pyplot.figure()
 variables = [0, 1, 2, 3]
 for v in variables:
-	pyplot.subplot(len(variables), 1, v+1)
-	pyplot.hist(all_rows[:, v], bins=20)
+    pyplot.subplot(len(variables), 1, v + 1)
+    pyplot.hist(all_rows[:, v], bins=20)
 pyplot.show()
 # histogram for trace lengths
 trace_lengths = [len(x) for x in sequences]
